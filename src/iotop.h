a25e78a5298adf5f0c448d538aba4cd9ad0bcfbc
Boian Bonev <bbonev@ipacct.com>
2020-10-12 03:35:12 +0300

    release 1.15

--- src/iotop.h
+++ src/iotop.h
@@ -27,7 +27,7 @@ You should have received a copy of the GNU General Public License along with thi
 #include <sys/types.h>
 #include <stdint.h>
 
-#define VERSION "1.15pre"
+#define VERSION "1.15"
 
 typedef union {
 	struct _flags {
