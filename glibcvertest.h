c2be63733538e45f98b79cc757a42b5770eaf856
Boian Bonev <bbonev@ipacct.com>
2021-09-23 04:00:46 +0300

    properly capitalize Linux

--- glibcvertest.h
+++ glibcvertest.h
@@ -13,7 +13,7 @@ You should have received a copy of the GNU General Public License along with thi
 
 /*
 
-iotop can only run on linux
+iotop can only run on Linux
 libc may vary, check if old glibc
 require -lrt for clock_gettime
 
