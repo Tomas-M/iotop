8cf2c05bfdcdf136343b021139d7c9c0213b2a00
Boian Bonev <bbonev@ipacct.com>
2022-01-26 17:59:39 +0200

    update copyright

--- src/iotop.h
+++ src/iotop.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifer: GPL-2.0-or-later
 
 Copyright (C) 2014  Vyacheslav Trushkin
-Copyright (C) 2020,2021  Boian Bonev
+Copyright (C) 2020-2022  Boian Bonev
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
@@ -27,7 +27,7 @@ You should have received a copy of the GNU General Public License along with thi
 #include <sys/types.h>
 #include <stdint.h>
 
-#define VERSION "1.21~pre2"
+#define VERSION "1.21"
 
 typedef enum {
 	E_GR_IO,
