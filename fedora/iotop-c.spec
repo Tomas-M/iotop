4e32789e496a027201e66b5773a9d7955f7c638b
Boian Bonev <bbonev@ipacct.com>
2025-01-13 12:40:50 +0200

    Prepare to release 1.27

--- fedora/iotop-c.spec
+++ fedora/iotop-c.spec
@@ -1,6 +1,6 @@
 Name:           iotop-c
-Version:        1.26
-Release:        2%{?dist}
+Version:        1.27
+Release:        1%{?dist}
 Summary:        Simple top-like I/O monitor (implemented in C)
 
 License:        GPL-2.0-or-later
@@ -53,6 +53,9 @@ V=1 STRIP=: %make_install
 %{_mandir}/man8/iotop.8*
 
 %changelog
+* Mon 13 Jan 2025 Boian Bonev <bbonev@ipacct.com> - 1.27-1
+- Update to latest ver 1.27
+
 * Thu Jul 18 2024 Fedora Release Engineering <releng@fedoraproject.org> - 1.26-2
 - Rebuilt for https://fedoraproject.org/wiki/Fedora_41_Mass_Rebuild
 
--- src/iotop.h
+++ src/iotop.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later
 
 Copyright (C) 2014  Vyacheslav Trushkin
-Copyright (C) 2020-2024  Boian Bonev
+Copyright (C) 2020-2025  Boian Bonev
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
@@ -30,7 +30,7 @@ You should have received a copy of the GNU General Public License along with thi
 
 #include "ucell.h"
 
-#define VERSION "1.26"
+#define VERSION "1.27"
 
 typedef enum {
 	E_GR_IO,
